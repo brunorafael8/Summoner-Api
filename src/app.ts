256eedb7809b42cea8f8b22cb8674d8bc37977eb
brunorafael8 <bruninho_lito@hotmail.com>
2019-06-05 19:13:48 -0300

    (add) fist config express on App src

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
-const summoner = "Welcome to Summoner's Rift"
+import express from 'express'
+const app = express()
 
-console.log(summoner)
+app.get('/', function (req, res): void {
+  res.send("Welcome to Summoner's Rift")
+})
+
+app.listen(3000, function (): void {
+  console.log('app listening on port 3000!')
+})
