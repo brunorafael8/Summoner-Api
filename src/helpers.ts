4e4c85e97849af9022dc8cf55674dac89014b458
brunorafael8 <bruninho_lito@hotmail.com>
2019-06-06 14:46:01 -0300

    (improve) helpers src

---
+++ src/helpers.ts
@@ -0,0 +1,53 @@
+import fetch from 'isomorphic-fetch'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export type Region =
+  | 'BR1'
+  | 'EUN1'
+  | 'EUW1'
+  | 'JP1'
+  | 'KR'
+  | 'LA1'
+  | 'LA2'
+  | 'NA1'
+  | 'OC1'
+  | 'RU'
+  | 'TR1';
+
+const BASE_URL = (region: Region): string =>
+  `https://${region.toLowerCase()}.api.riotgames.com`
+
+const HEADER_RIOT_TOKEN = 'X-Riot-Token'
+
+export interface PromiseResponse {
+  region: Region;
+  name: string;
+}
+
+export const fetchSummonerByName = (region: Region, name: string): Promise<PromiseResponse> => {
+  return fetch(
+    `${BASE_URL(region)}/lol/summoner/v4/summoners/by-name/${name}`,
+    {
+      headers: {
+        [HEADER_RIOT_TOKEN]: process.env.API_KEY
+      },
+      method: 'GET'
+    }
+  )
+    .then(response => {
+      console.log(process.env.API_KEY)
+      if (response.status >= 400) {
+        throw new Error('Bad response from server')
+      }
+
+      return response.json().then((result) => {
+        console.log(result)
+        return result
+      }).catch((err) => {
+        console.log('error', err)
+      })
+    })
+    .catch(error => console.log('error', error))
+}
